ea76c8e6d2ca49d0de514833955b312e66af3fe5
andrewvect <86428962+andrewvect@users.noreply.github.com>
2025-03-07 19:30:24 +0100

    🧪 Refactor database session management to use asynchronous SQLAlchemy for improved performance and scalability

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -4,9 +4,9 @@ from typing import Annotated
 import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jwt.exceptions import InvalidTokenError
-from pydantic import ValidationError
-from sqlmodel import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.core import security
 from app.core.config import settings
@@ -18,12 +18,25 @@ reusable_oauth2 = OAuth2PasswordBearer(
 )
 
 
-def get_db() -> Generator[Session, None, None]:
-    with Session(engine) as session:
+async def get_session() -> AsyncIterator[AsyncSession]:
+    try:
+        engine = create_async_engine(
+            str(settings.ASYNC_SQLALCHEMY_DATABASE_URI),
+            pool_size=20,
+            max_overflow=10,
+            pool_recycle=3600  # recycle connections after 1 hour
+        )
+
+        async_session = sessionmaker(
+            engine, class_=AsyncSession, expire_on_commit=False, future=True
+        )
+        session = async_session()
         yield session
+    finally:
+        await session.close()
 
 
-SessionDep = Annotated[Session, Depends(get_db)]
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
 TokenDep = Annotated[str, Depends(reusable_oauth2)]
 
 
