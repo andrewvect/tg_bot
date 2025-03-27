9c8b9bc2ebe528f21c77016742d90032fdaadf4b
andrewvect <86428962+andrewvect@users.noreply.github.com>
2025-03-27 12:19:13 +0100

    Add Settings model to manage user-specific settings

---
+++ backend/app/common/db/models/settings.py
@@ -0,0 +1,50 @@
+import sqlalchemy as sa
+from sqlalchemy import CheckConstraint, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+
+class Settings(Base):
+    """
+    Settings model representing user-specific settings in the system.
+    """
+
+    spoiler_settings: Mapped[int] = mapped_column(
+        sa.Integer,
+        nullable=False,
+        default=1,
+    )
+    """ User's spoiler setting (enabled=1, disabled=0) """
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.telegram_id"),
+        nullable=False,
+        index=True,
+        unique=True,
+        doc="Foreign key to User's telegram_id",
+    )
+    """ Foreign key to User's telegram_id """
+
+    # Constrains
+    __table_args__ = (
+        CheckConstraint(
+            "spoiler_settings IN (0, 1, 2, 3, 4)", name="check_spoiler_settings"
+        ),
+    )
+    """ Ensure spoiler_settings"""
+
+    # Relationships
+    user: Mapped["User"] = relationship(  # noqa
+        "User",
+        back_populates="settings",
+        uselist=False,
+        doc="Relationship to the associated User",
+    )
+    """ Relationship to the associated User """
+
+    def __str__(self):
+        return str(self.spoiler_settings)
+
+    def __repr__(self):
+        return f"<Settings(id={self.id}, spoiler_settings={self.spoiler_settings})>"
