b34fdc6d92c426d2b83960db7280d93ba042bb15
andrewvect <86428962+andrewvect@users.noreply.github.com>
2025-03-27 12:51:41 +0100

    Add CardComponent for reusable card layout with customizable header

---
+++ frontend/src/components/Card/card.tsx
@@ -0,0 +1,23 @@
+import { Card, CardBody, CardHeader, Text, Center, VStack } from "@chakra-ui/react"
+
+interface CardComponentProps {
+    header: string;
+    children: React.ReactNode;
+}
+
+export function CardComponent({ header, children }: CardComponentProps) {
+    return (
+        <Card width="300px" backgroundColor="black">
+            <CardBody gap="2">
+                <Center>
+                    <VStack>
+                        <CardHeader>
+                            <Text fontSize="5xl" fontWeight="bold" color="white">{header}</Text>
+                        </CardHeader>
+                        {children}
+                    </VStack>
+                </Center>
+            </CardBody>
+        </Card>
+    )
+}
