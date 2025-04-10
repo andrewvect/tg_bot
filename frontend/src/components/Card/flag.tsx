14683e29759e59f91223dfb9abbe910f9116113c
andrewvect <86428962+andrewvect@users.noreply.github.com>
2025-04-10 16:34:25 +0200

    add Flag component to display emoji within a circular layout

---
+++ frontend/src/components/Card/flag.tsx
@@ -0,0 +1,15 @@
+import { Circle, Box } from "@chakra-ui/react"
+
+interface FlagProps {
+  emoji: string;
+}
+
+export const Flag = ({ emoji }: FlagProps) => {
+    return (
+        <Box >
+        <Circle size="6" top="15vh" left="15vw" position="absolute">
+            <span style={{ fontSize: '30px' }}>{emoji}</span>
+        </Circle>
+        </Box>
+    )
+}
