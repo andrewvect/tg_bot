13ab9b2d1edd837ee81bc51dea65c9dc11068bb1
andrewvect <86428962+andrewvect@users.noreply.github.com>
2025-04-15 11:16:30 +0200

    feat: enhance Legend component to display sentences with a header in popover

--- frontend/src/components/Card/legend.tsx
+++ frontend/src/components/Card/legend.tsx
@@ -1,26 +1,45 @@
-import { Button, Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody, Circle, Box } from "@chakra-ui/react"
+import { Button, Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody, Circle, Box, Text } from "@chakra-ui/react"
+import Sentences from './sentences';
+import { WordResponse } from '../../client/types.gen'
 
 interface LegendProps {
-	legend: string
+	sentences: WordResponse[],
+	header: string
 }
 
-export const Legend = ({ legend }: LegendProps) => {
-	// If legend is null, do not show the element.
-	if (legend === null) return null;
+export const Legend = ({ sentences, header }: LegendProps) => {
+
 	return (
 		<Box>
-		<Circle size="6" position="absolute" top="15vh" right="15vw">
-			<Popover>
-				<PopoverTrigger>
-					<Button size="2xl" fontSize="2xl" colorScheme="white">?</Button>
-				</PopoverTrigger>
-				<PopoverContent bg="black">
-					<PopoverArrow bg="black" />
-					<PopoverCloseButton color="white" />
-					<PopoverBody color="white">{legend}</PopoverBody>
-				</PopoverContent>
-			</Popover>
-		</Circle>
+			<Circle size="6" position="absolute" top="15vh" right="15vw">
+				<Popover>
+					<PopoverTrigger>
+						<Button size="2xl" fontSize="2xl" colorScheme="white">?</Button>
+					</PopoverTrigger>
+					<PopoverContent
+						bg="black"
+						maxW="90vw"
+						maxH="80vh"
+						overflow="hidden"
+						mr="5vw"
+					>
+						<PopoverArrow bg="black" />
+						<PopoverCloseButton color="white" />
+						<PopoverBody
+							color="white"
+							overflowY="auto"
+							maxH="60vh"
+						>
+							<Text mb={2}>{header}</Text>
+							{sentences && sentences.length > 0 && (
+								<>
+									<Sentences sentences={sentences[0]?.sentences ?? []} />
+								</>
+							)}
+						</PopoverBody>
+					</PopoverContent>
+				</Popover>
+			</Circle>
 		</Box>
 	)
 }
