a79a0f53103257ef790176be64609faf7dd13a4f
andrewvect <86428962+andrewvect@users.noreply.github.com>
2025-04-25 11:18:01 +0200

    fix: change known_cards and master_cards types from SortedList to SortedSet for consistency

--- backend/app/common/cache/states.py
+++ backend/app/common/cache/states.py
@@ -10,13 +10,13 @@ class UserProfile:
     # store words ids which user passed
     created_cards: SortedList = field(default_factory=SortedSet)
     # store words ids which user marked as known
-    known_cards: SortedList = field(default_factory=SortedSet)
+    known_cards: SortedSet = field(default_factory=SortedSet)
     # store words ids which user makes 10 right reviews
-    master_cards: SortedList = field(default_factory=SortedSet)
+    master_cards: SortedSet = field(default_factory=SortedSet)
     # store words ids which to review
     review_cards: list = field(default_factory=list)
     # store words ids which user is waiting to review
     waiting_cards: SortedDict = field(default_factory=SortedDict)
 
 
-users_states = {}
+users_states: dict[int, UserProfile] = {}
